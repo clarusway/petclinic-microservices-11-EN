9c9000f29fd7568392fffba77dc2d0ec8a467966
AlexDuncan2100 <alexad2100@gmail.com>
2022-09-30 12:47:41 +0000

    added Jenkins Job for CI pipeline

--- jenkins/jenkins-petclinic-ci-job.sh
+++ jenkins/jenkins-petclinic-ci-job.sh
@@ -1,2 +1,2 @@
-echo 'Running Unit Tests on Petclinic Application'
+echo 'Running Unit Tests on petclinic Application'
 docker run --rm -v $HOME/.m2:/root/.m2 -v `pwd`:/app -w /app maven:3.8-openjdk-11 mvn clean test
