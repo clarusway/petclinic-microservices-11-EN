36ba8bbec05e704974d6eaeba3006f3242d01cf4
AlexDuncan2100 <alexad2100@gmail.com>
2022-10-02 19:21:02 +0000

    ci-test commit

--- jenkins/jenkins-petclinic-ci-job.sh
+++ jenkins/jenkins-petclinic-ci-job.sh
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-echo 'Running Unit Tests on Petclinic Applications'
-=======
 echo 'Running Unit Tests on petclinic Application'
->>>>>>> feature/msp-14
 docker run --rm -v $HOME/.m2:/root/.m2 -v `pwd`:/app -w /app maven:3.8-openjdk-11 mvn clean test
